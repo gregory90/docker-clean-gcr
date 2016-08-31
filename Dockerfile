e7066bd7c38ec9efec2b59e25b89d845a6a714ce
Grzegorz Graczyk <gregory90@gmail.com>
2016-08-31 13:19:50 +0200

    initial

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 FROM sdurrheimer/alpine-glibc
 
-ENV KUBECTL_VERSION 1.3.6
-ENV \
-    BUILD_DEPS="gettext"  \
-    RUNTIME_DEPS="libintl"
-
 # install python
 RUN echo "@testing http://dl-4.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories \
   && apk add --update \
@@ -27,11 +22,6 @@ RUN cd /usr/bin \
 
 # Install kubectl
 RUN apk add --update -t deps curl ca-certificates \
- && curl -L https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl \
- && chmod +x /usr/local/bin/kubectl \
- && apk add --update $RUNTIME_DEPS \
- && apk add --virtual build_deps $BUILD_DEPS \
- && cp /usr/bin/envsubst /usr/local/bin/envsubst \
  && curl -L https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz -o ./google-cloud-sdk.tar.gz \
  && tar zxvf google-cloud-sdk.tar.gz \
  && rm google-cloud-sdk.tar.gz \
@@ -48,10 +38,7 @@ ENV PATH /google-cloud-sdk/bin:$PATH
 RUN y | gcloud components update
 RUN y | gcloud components update alpha
 
-COPY ./envsubst.sh /envsubst.sh
-RUN chmod +x /envsubst.sh
+COPY ./entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
 
-COPY ./deploy /deploy
-RUN chmod +x /deploy
-
-CMD ["kubectl"]
+ENTRYPOINT ["entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# clean repository
+# authenticate to google cloud
+echo "Authenticating to Google Cloud..."
+echo $DOCKER_KEYFILE > /key.json
+/google-cloud-sdk/bin/gcloud auth activate-service-account "$GCLOUD_ACCOUNT" --key-file /key.json --project "$GCLOUD_PROJECT" -q
+
+echo "Getting first level of repos..."
+# first level of repos
+im=$(/google-cloud-sdk/bin/gcloud alpha container images list --repository=eu.gcr.io/pq-infrastructure)
+IFS=$'\n' read -rd '' -a images <<<"$im"
+
+for image in "${images[@]}"
+do
+  if [ $image == "NAME" ]; then
+    continue
+  fi
+
+  echo "Repo found: $image"
+  # second level of repos
+  im2=$(/google-cloud-sdk/bin/gcloud alpha container images list --repository=$image)
+
+  IFS=$'\n' read -rd '' -a images2 <<<"$im2"
+  for image2 in "${images2[@]}"
+  do
+    if [ $image2 == "NAME" ]; then
+      continue
+    fi
+    echo "Sub-repo found: $image2"
+
+    # tags
+    ts=$(/google-cloud-sdk/bin/gcloud alpha container images list-tags $image2 | sort --reverse -k 3,3 | tail -n +6 | head -n -1)
+
+    IFS=$'\n' read -rd '' -a tags <<<"$ts"
+    for tag in "${tags[@]}"
+    do
+      t=$(echo "$tag" | awk '{print $2}')
+      echo "Deleting $image2:$t"
+      echo 'y' | gcloud alpha container images delete $image2:$t
+
+    done
+
+  done
+
+done
