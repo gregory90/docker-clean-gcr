365d839a4a82ffa6677a2b7eabcd90dd95910a58
Grzegorz Graczyk <gregory90@gmail.com>
2016-09-01 14:09:25 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Authenticating to Google Cloud..."
 echo $DOCKER_KEYFILE_BASE64 | base64 --decode > /key.json
 gcloud auth activate-service-account --key-file /key.json --project "$GCLOUD_PROJECT" -q
 
-echo "\nGetting first level of repos..."
+echo "Getting first level of repos..."
 # first level of repos
 im=$(gcloud alpha container images list --repository=eu.gcr.io/pq-infrastructure)
 IFS=$'\n' read -rd '' -a images <<<"$im"
@@ -15,7 +15,7 @@ do
     continue
   fi
 
-  echo "\nRepo found: $image"
+  echo "Repo found: $image"
   # second level of repos
   im2=$(gcloud alpha container images list --repository=$image)
 
@@ -25,7 +25,7 @@ do
     if [ $image2 == "NAME" ]; then
       continue
     fi
-    echo "\nSub-repo found: $image2"
+    echo "Sub-repo found: $image2"
 
     # tags
     ts=$(gcloud alpha container images list-tags $image2 | sort --reverse -k 3,3 | tail -n +6 | head -n -1)
@@ -35,7 +35,7 @@ do
     do
       t=$(echo "$tag" | awk '{print $2}')
       echo "Deleting $image2:$t"
-      echo 'y' | gcloud alpha container images delete $image2:$t
+      echo 'y' | gcloud alpha container images delete $image2:$t -q
 
     done
 
