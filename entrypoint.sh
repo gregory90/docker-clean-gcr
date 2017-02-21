c6ca997ccc54918b55170e2c75af64ae238e58d6
Grzegorz Graczyk <gregory90@gmail.com>
2017-02-21 09:07:08 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ gcloud auth activate-service-account --key-file /key.json --project "$GCLOUD_PRO
 
 echo "Getting first level of repos..."
 # first level of repos
-im=$(gcloud alpha container images list --repository=eu.gcr.io/pq-infrastructure)
+im=$(gcloud beta container images list --repository=eu.gcr.io/pq-infrastructure)
 IFS=$'\n' read -rd '' -a images <<<"$im"
 
 for image in "${images[@]}"
@@ -17,7 +17,7 @@ do
 
   echo "Repo found: $image"
   # second level of repos
-  im2=$(gcloud alpha container images list --repository=$image)
+  im2=$(gcloud beta container images list --repository=$image)
 
   IFS=$'\n' read -rd '' -a images2 <<<"$im2"
   for image2 in "${images2[@]}"
@@ -28,14 +28,14 @@ do
     echo "Sub-repo found: $image2"
 
     # tags
-    ts=$(gcloud alpha container images list-tags $image2 | sort --reverse -k 3,3 | tail -n +6 | head -n -1)
+    ts=$(gcloud beta container images list-tags $image2 | sort --reverse -k 3,3 | tail -n +6 | head -n -1)
 
     IFS=$'\n' read -rd '' -a tags <<<"$ts"
     for tag in "${tags[@]}"
     do
       t=$(echo "$tag" | awk '{print $1}')
       echo "Deleting $image2@sha256:$t"
-      echo 'y' | gcloud alpha container images delete $image2@sha256:$t -q
+      echo 'y' | gcloud beta container images delete $image2@sha256:$t -q
 
     done
 
